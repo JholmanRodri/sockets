118dc898a6d151210aa3ad1f44a3479a3815f979
APRENDIZ <APRENDIZ@SGLDFPCATSD246.FormacionSena.RED>
2022-12-15 22:25:13 -0500

    segundo

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,17 @@
-const txtNombre= document.querySelector("#txtNombre")
-const btnSaludar= document.querySelector("#btnSaludar")
-const txtMensaje= document.querySelector("#txtMensaje")
-const btnMensaje= document.querySelector("#btnMensaje")
+// const txtNombre= document.querySelector("#txtNombre")
+// const btnSaludar= document.querySelector("#btnSaludar")
+// const txtMensaje= document.querySelector("#txtMensaje")
+// const btnMensaje= document.querySelector("#btnMensaje")
+
+const btnMensaje2 = document.querySelector("#btnMensaje2")
+const txtNombre2 = document.querySelector("#txtNombre2")
+const txtMensaje2 = document.querySelector("#txtMensaje2")
+
+const fecha = new Date();
+const fecha2 = fecha.toLocaleString()
 
 var socket = io();
+let Chat = [];
 // on escuchar 
 
 socket.on("connect", function () {
@@ -18,27 +26,55 @@ socket.on("buenas", function (msg) {
   console.log(`Este es el mensaje del servidor ${msg}`);
 });
 
-socket.on("notificar", function (msg) {
-  console.log(msg);
-});
+// socket.on("notificar", function (msg) {
+//   console.log(msg);
+// });
 
 //emit hablo 
 
-btnSaludar.addEventListener("click",()=>{
-    socket.emit("holaTodos", `Saludo de ${txtNombre.value}`, function (rtaServidor) {
-        console.log(rtaServidor);
+// btnSaludar.addEventListener("click",()=>{
+//     socket.emit("holaTodos", `Saludo de ${txtNombre.value}`, function (rtaServidor) {
+//         console.log(rtaServidor);
+//       });
+// })
+
+//Mensajes
+
+btnMensaje2.addEventListener("click",()=>{
+    let a = txtNombre2.value.toUpperCase()  
+    socket.emit("mensajeTodos", `${fecha2}/  ${a} : ${txtMensaje2.value}`, function (rtaServidor) {
+        let mensaje1 = `<li>${fecha2} ${a} ${txtMensaje2.value}</li>`
+        Chat.push(mensaje1)
+      for (let i=0; i<Chat.length; i++){
+        document.getElementById("mensaje").innerHTML = Chat
+      }
+        // document.getElementById("mensaje").innerHTML = rtaServidor
+        // console.log(b);
+        // console.log(rtaServidor);
       });
 })
 
-btnMensaje.addEventListener("click",()=>{
-    socket.emit(
-        "enviarMensaje",
-        {
-          nombre: "Mariana",mensaje: txtMensaje.value,
-        },
-        function (resp) {
-          console.log(`Esto es una respuesta del servidor ${resp.msg}`);
-        }
-      );
+socket.on("notificar", function(msg){
+
+  let mensaje1 = `<li>${msg}</li>`
+
+  Chat.push(mensaje1)
+  for (let i=0; i<Chat.length; i++){
+  document.getElementById("mensaje").innerHTML = Chat
+  }
+
+  console.log(mensaje1);
 })
+
+// btnMensaje.addEventListener("click",()=>{
+//     socket.emit(
+//         "enviarMensaje",
+//         {
+//           nombre: "Mariana",mensaje: txtMensaje.value,
+//         },
+//         function (resp) {
+//           console.log(`Esto es una respuesta del servidor ${resp.msg}`);
+//         }
+//       );
+// })
 
--- sockets/controller.js
+++ sockets/controller.js
@@ -1,22 +1,29 @@
 
 const socketController =(socket)=>{
     console.log(socket.id);
-    socket.broadcast.emit('notificar',`Nuevo usario ${socket.id}`)
+    socket.broadcast.emit('notificarLogin',`Nuevo usario ${socket.id}`)
     
-    socket.on('enviarMensaje',function(usuario,callback){
-        console.log(usuario.nombre);
-        console.log(usuario.mensaje);
-        callback({
-            msg:"Usuario insertado en la bd",
-            usuario
-        })
-    })
+    // socket.on('enviarMensaje',function(usuario,callback){
+    //     console.log(usuario.nombre);
+    //     console.log(usuario.mensaje);
+    //     callback({
+    //         msg:"Usuario insertado en la bd",
+    //         usuario
+    //     })
+    // })
 
-    socket.on('holaTodos',function(msg,callback){
+    // socket.on('holaTodos',function(msg,callback){
+    //     console.log(msg);
+    //     socket.broadcast.emit('notificar',msg)
+    //     callback('Mensaje enviado')
+    // })
+
+    socket.on('mensajeTodos',function(msg,callback){
         console.log(msg);
         socket.broadcast.emit('notificar',msg)
         callback('Mensaje enviado')
     })
+
 }
 
-export default socketController
+export default socketController 
